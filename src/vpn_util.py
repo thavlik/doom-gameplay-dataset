35310d0c0fd02733e23bc8e66a73920025a049f0
Tom Havlik <keres993@gmail.com>
2020-12-09 10:54:48 -0600

    ...

--- src/vpn_util.py
+++ src/vpn_util.py
@@ -1,8 +1,7 @@
 import subprocess
 
 
-def connect():
-    cmd = 'nordvpn connect'
+def run_cmd(cmd: str):
     proc = subprocess.run(
         ['bash', '-c', cmd], capture_output=True)
     if proc.returncode != 0:
@@ -13,13 +12,11 @@ def connect():
         raise ValueError(msg)
 
 
+def connect():
+    run_cmd('nordvpn connect')
+
+
 def reconnect():
-    cmd = 'nordvpn disconnect'
-    proc = subprocess.run(
-        ['bash', '-c', cmd], capture_output=True)
-    if proc.returncode != 0:
-        msg = 'expected exit code 0 from `{}`, got exit code {}: {}'.format(
-            cmd, proc.returncode, proc.stdout.decode('unicode_escape'))
-        if proc.stderr:
-            msg += ' ' + proc.stderr.decode('unicode_escape')
-    connect()
+    print('Reconnecting to NordVPN...')
+    run_cmd('nordvpn disconnect')
+    run_cmd('nordvpn connect')
