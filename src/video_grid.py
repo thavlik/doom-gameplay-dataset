8b49d367c35e78b5b63a4fad1a33fdf0238a1e35
Tom Havlik <keres993@gmail.com>
2020-12-15 07:43:13 -0600

    ...

--- src/video_grid.py
+++ src/video_grid.py
@@ -8,41 +8,56 @@ import subprocess
 
 bridge.set_bridge('torch')
 
-input = '/mnt/disk/640x480'
+input = '/mnt/e/quake-gameplay-dataset/download/320x240'
 rows = 4
 cols = 4
 fps = 12
 num_seconds = 8
 num_frames = fps * num_seconds
 limit = rows * cols
-files = sorted(f for f in os.listdir(input)
-               if f.endswith('1.mp4'))
-indices = np.random.randint(0, len(files), size=(1, limit)).squeeze()
+id_len = len('-9PA3A7h1KE')
+using = {}
+files = []
+for f in os.listdir(input):
+    # Make sure we do not use the start of any video
+    if f.endswith('.mp4') and not f.endswith('0.mp4'):
+        id = f[:id_len]
+        if id in using:
+            # Do not use the same video more than once
+            print(f'Skipping {id}')
+            continue
+        using[id] = True
+        files.append(f)
+        print(f'Using video {f}')
+sorted_files = sorted(files)
+files = sorted_files
+indices = np.random.choice(len(files), limit, replace=False)
 files = [files[idx] for idx in indices]
-print(f'Using videos {files}')
+print(f'Using {len(using)} videos: {files}')
+assert len(set(files)) == len(files)
 videos = [VideoReader(os.path.join(input, f)) for f in files]
 frames = np.array([[vr.next().numpy() for _ in range(num_frames)]
                    for vr in videos])
-for frame in range(num_frames):
+for frameno in range(num_frames):
     i = 0
     complete_frame = []
     for _ in range(rows):
         items = []
         for _ in range(cols):
-            frame_data = frames[i][frame]
+            frame_data = frames[i][frameno]
             items.append(frame_data)
             i += 1
         items = np.concatenate(items, axis=1)
         complete_frame.append(items)
     complete_frame = np.concatenate(complete_frame, axis=0)
     img = Image.fromarray(complete_frame)
-    img = img.resize((img.width//4, img.height//4))
-    out_path = 'frame_{0:04}.png'.format(frame)
+    img = img.resize((img.width//2, img.height//2))
+    out_path = 'frame_{0:04}.png'.format(frameno)
     img.save(out_path)
     print(f'Wrote {out_path}')
-out_path = 'output'
+out_path = 'thumbnails'
 print(f'Encoding video to {out_path}.mp4')
-cmd = f"ffmpeg -r {fps} -s {img.width}x{img.height} -i frame_%04d.png -crf 25 -pix_fmt yuv420p {out_path}.mp4"
+cmd = f"ffmpeg -y -r {fps} -s {img.width}x{img.height} -i frame_%04d.png -crf 25 -pix_fmt yuv420p {out_path}.mp4"
 print(f'Running {cmd}')
 proc = subprocess.run(
     ['bash', '-c', cmd], capture_output=True)
@@ -52,7 +67,7 @@ if proc.returncode != 0:
     if proc.stderr:
         msg += ' ' + proc.stderr.decode('unicode_escape')
     raise ValueError(msg)
-cmd = f"ffmpeg -i {out_path}.mp4 {out_path}.gif"
+cmd = f"ffmpeg -y -i {out_path}.mp4 {out_path}.gif"
 print(f'Running {cmd}')
 proc = subprocess.run(
     ['bash', '-c', cmd], capture_output=True)
@@ -63,4 +78,4 @@ if proc.returncode != 0:
         msg += ' ' + proc.stderr.decode('unicode_escape')
     raise ValueError(msg)
 print(f'Wrote gif to {out_path}.gif')
-[os.remove(f'frame_{i}.png') for i in range(num_frames)]
+[os.remove(f'frame_' + f'{i}.png'.rjust(8, '0')) for i in range(num_frames)]
